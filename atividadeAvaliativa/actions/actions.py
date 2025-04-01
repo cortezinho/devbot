756bb0d692d8260813f9584dab9b8a81e7f4b4cd
MykyBalb <mykaelbalbino06@gmail.com>
2025-04-01 19:57:24 -0300

    Atividade Avaliativa

--- atividadeAvaliativa/actions/actions.py
+++ atividadeAvaliativa/actions/actions.py
@@ -15,39 +15,20 @@ class ActionFornecerSuporte(Action):
         
         if intent == "problema_acesso":
             resposta = """Para problemas de acesso:
-1. Clique em 'Esqueci minha senha'
-2. Verifique seu e-mail
-3. Crie uma nova senha
-Mais informações: [Ajuda com acesso](https://somnanuvem.ajuda/acesso)"""
+Verifique seu email, senha e telefone..."""
         
         elif intent == "problema_plano":
             resposta = """Para alterar seu plano:
-1. Acesse 'Minha Conta'
-2. Selecione 'Planos'
-3. Escolha seu novo plano
-
-Planos disponíveis:
-- Básico: R$19,90/mês
-- Premium: R$29,90/mês
-- Família: R$39,90/mês (até 6 pessoas)
-
-Mais informações: [Planos disponíveis](https://somnanuvem.ajuda/planos)"""
+Para alterar seu plano vá em configurações e em seus planos e mude pra outro.
+"""
         
         elif intent == "problema_tecnico":
             resposta = """Para problemas técnicos:
-1. Reinicie o aplicativo
-2. Verifique sua conexão com a internet
-3. Atualize para a versão mais recente
-
-Se o problema persistir, posso encaminhar para um atendente humano."""
+Busque verificar seu aplicativo, caso não funcione entre em contato"""
         
         elif intent == "problema_pagamento":
             resposta = """Para problemas com pagamento:
-1. Verifique os dados do seu cartão
-2. Confira o limite disponível
-3. Tente outro método de pagamento
-
-Para ajustes de cobrança, precisaremos transferir para um atendente humano."""
+Verifique seus dados, numero do cartao e cvv."""
         
         else:
             resposta = "Não entendi completamente seu problema. Poderia reformular?"
